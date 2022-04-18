3aad5e03a721fcd470277bc5fd3007b03c4282fc
Kochnev <dkochnev@rencap.com>
2022-04-19 02:16:40 +0300

    inc8#commit1# body limit

--- internal/handler/zippo/zippo.go
+++ internal/handler/zippo/zippo.go
@@ -2,12 +2,17 @@ package zippo
 
 import (
 	"github.com/labstack/echo/v4"
+	"strings"
 )
 
 func DelContentLength() echo.MiddlewareFunc {
 	return func(next echo.HandlerFunc) echo.HandlerFunc {
 		return func(c echo.Context) error {
-			c.Response().Header().Del(echo.HeaderContentLength)
+			if strings.Contains(c.Request().Header.Get(echo.HeaderAcceptEncoding), "gzip") {
+				c.Response().Header().Set(echo.HeaderContentEncoding, "gzip")
+				c.Response().Header().Set(echo.HeaderVary, "Accept-Encoding")
+				c.Response().Header().Del(echo.HeaderContentLength)
+			}
 			return next(c)
 		}
 	}
